2ee62e8dd7a2420a853faf76b1d4315d0654f74c
RezaAkbar-dev <rezaakbar6702003@gmail.com>
2026-07-06 11:56:15 +0800

    init file

---
+++ src/data/cardsData.js
@@ -0,0 +1,16 @@
+const initialCards = [
+    {id:1, name:"Pikachu", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:2, name:"Charmander", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:3, name:"Squirtle", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:4, name:"Bulbasaur", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:5, name:"Eevee", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:6, name:"Snorlax", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:7, name:"Mewtwo", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:8, name:"Gengar", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:9, name:"Jigglypuff", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:10, name:"Psyduck", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:11, name:"Lucario", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
+    {id:12, name:"Gyarados", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"}
+];
+
+export default initialCards;
