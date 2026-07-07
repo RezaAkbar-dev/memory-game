c509147ed5b23b5a8be488d9e689f65db2d8ad20
RezaAkbar-dev <rezaakbar6702003@gmail.com>
2026-07-07 08:26:29 +0800

    on proggres logic tampilan kartu

--- src/App.jsx
+++ src/App.jsx
@@ -2,121 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import './styles/App.css'
 import initialCards from './data/cardsData'
+import { shuffleArray } from './utils/shuffle'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState(initialCards);
+  const [score,setScore] = useState(0);
+  const [highScore,setHighScore] = useState(0);
+  const [clickedCards,setClickedCards] = useState([]);
+
+  // function handleCardClick(id){
+  //   if(id.include(clickedCard)){
+  //     setScore(0);
+  //     shuffleArray(cards);
+  //     clickedCards([]);
+  //     setHighScore(score);
+  //     return;
+  //   }
+  //   setScore(score + 1)
+  //   shuffleArray(cards);
+  //   clickedCard([...clickedCard,id]);
+  // }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1>Poke-Memory Card Game</h1>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <div>
+          {cards.map((card) => {
+            return (
+              <div key={card.id}>
+                <img src={card.image} alt={card.name} />
+                <p>{card.name}</p>
+              </div>
+            )
+          })}
+      </div>
+    </div>
   )
 }
 
--- src/data/cardsData.js
+++ src/data/cardsData.js
@@ -1,16 +1,16 @@
 const initialCards = [
     {id:1, name:"Pikachu", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:2, name:"Charmander", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:3, name:"Squirtle", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:4, name:"Bulbasaur", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:5, name:"Eevee", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:6, name:"Snorlax", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:7, name:"Mewtwo", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:8, name:"Gengar", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:9, name:"Jigglypuff", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:10, name:"Psyduck", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:11, name:"Lucario", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"},
-    {id:12, name:"Gyarados", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"}
+    {id:2, name:"Charmander", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"},
+    {id:3, name:"Squirtle", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"},
+    {id:4, name:"Bulbasaur", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"},
+    {id:5, name:"Eevee", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png"},
+    {id:6, name:"Snorlax", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png"},
+    {id:7, name:"Mewtwo", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png"},
+    {id:8, name:"Gengar", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png"},
+    {id:9, name:"Jigglypuff", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png"},
+    {id:10, name:"Psyduck", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/54.png"},
+    {id:11, name:"Lucario", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/448.png"},
+    {id:12, name:"Gyarados", image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/130.png"}
 ];
 
 export default initialCards;
